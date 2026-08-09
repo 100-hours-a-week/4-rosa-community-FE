d0bf05b1a9acd5d448311f00988ce496a99eeb29
jungyungee <jungyungee0610@gmail.com>
2026-08-09 17:26:52 +0900

    #10 [FEAT] 게시글 카테고리 API 요청 추가

--- api/indexRequest.js
+++ api/indexRequest.js
@@ -1,8 +1,12 @@
 import { getServerUrl } from '../utils/function.js';
 import { requestJson } from '../utils/request.js';
 
-export const getPosts = cursor => {
-    const query = cursor ? `?cursor=${encodeURIComponent(cursor)}` : '';
+export const getPosts = (cursor, categoryCode = null) => {
+    const params = new URLSearchParams();
+    if (cursor) params.set('cursor', cursor);
+    if (categoryCode) params.set('categoryCode', categoryCode);
+
+    const query = params.toString() ? `?${params.toString()}` : '';
     const result = requestJson(`${getServerUrl()}/posts${query}`);
     return result;
 };
